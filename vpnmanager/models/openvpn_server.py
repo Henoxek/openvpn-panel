6b9a831262c52346eb049da8ddcec9647fa17f82
Henoxek <me@henoxek.ru>
2015-06-28 21:59:50 +0300

    more impl

--- vpnmanager/models/openvpn_server.py
+++ vpnmanager/models/openvpn_server.py
@@ -1,5 +1,14 @@
 import docker
 import json
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from exceptions import ServerNotFoundException
+
+
+db = MongoClient().vpnmanager
+db.servers.ensure_index('name', unique=True)
+db.servers.ensure_index('is_deleted')
+db.events.ensure_index('type')
 
 
 class ServerAlreadyExistsException(Exception):
@@ -36,21 +45,53 @@ class OpenVPNServer(object):
         }
 
     def save(self):
-        raise NotImplementedError
+        if self.id is None:
+            _id = db.servers.insert({
+                'name': self.name,
+                'protocol': self.protocol,
+                'port': self.port,
+                'wan_routing': self.wan_routing,
+                'running': self.running,
+                'is_deleted': False
+            })
+            self.id = str(_id)
+            db.events.insert({
+                'type': 'create_server',
+                'server_id': _id
+            })
+        else:
+            raise NotImplementedError
 
     def delete(self):
-        raise NotImplementedError
+        if self.id is None:
+            db.servers.update({'_id': ObjectId(self.id)}, {
+                '$set': {'is_deleted': True}
+            })
 
     def _load(self):
-        raise NotImplementedError
+        srv = db.servers.find_one({'$and': [{'_id': ObjectId(self.id)},
+                                            {'is_deleted': False}]})
+        if srv is None:
+            raise ServerNotFoundException(self.id)
+        self.protocol = srv['protocol']
+        self.port = srv['port']
+        self.wan_routing = srv['wan_routing']
+        self.running = srv['running']
+        self.name = srv['name']
 
     @staticmethod
     def find_all():
-        raise NotImplementedError
+        srvs = [srv for srv in db.servers.find({'is_deleted': False})]
+        return [OpenVPNServer(str(srv['_id'])) for srv in srvs]
 
     @staticmethod
     def find(server_id):
-        raise NotImplementedError
+        srv = db.servers.find_one({'$and': [{'is_deleted': False},
+                                            {'_id': ObjectId(server_id)}]
+                                   })
+        if srv is None:
+            raise ServerNotFoundException(server_id)
+        return OpenVPNServer(srv['_id'])
 
     @property
     def clients(self):
